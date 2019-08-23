358ac05d75ec4b65539bfeb55b4b04f8dcd91a20
yildirim <ziya.yildirim@akinon.com>
2019-08-23 21:04:58 +0300

    Swipper silder  add

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,11 @@
 import '../scss/base.scss';
 import $ from "jquery";
 import anime from 'animejs/lib/anime.es.js';
+import Swiper from 'swiper';
 
+var mySwiper = new Swiper('.swiper-container', { /* ... */ });
+
+console.log(mySwiper)
 const sayHello = ()=> {
   setInterval(() => {
     console.log('nasilsin ziya')
