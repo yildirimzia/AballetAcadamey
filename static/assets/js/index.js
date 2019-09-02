05717f159ed99540e8ce20fe3b7dfd27a15539c3
yildirim <ziya.yildirim@akinon.com>
2019-09-02 21:44:28 +0300

    footer Calismasi

--- static/assets/js/index.js
+++ static/assets/js/index.js
@@ -2,7 +2,6 @@ import '../scss/base.scss';
 import $ from "jquery";
 import anime from 'animejs/lib/anime.es.js';
 import Swiper from 'swiper';
-
 var mySwiper = new Swiper('.swiper-container', { /* ... */ });
 
 console.log(mySwiper)
