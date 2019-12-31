fb9fbb7ee43d6737c0e9a89999ec82f8790301ba
yildirim <ziya.yildirim@akinon.com>
2019-12-31 15:38:12 +0300

    working

--- static/assets/js/index.js
+++ static/assets/js/index.js
@@ -4,6 +4,29 @@ import '../scss/base.scss';
 
 
 
+const backToTop = () => {
+  let lastScrollTop = 0;
+  const backToTop =  $('.js-back-to-top');
+  $(window).scroll(()=> {
+    let scTop = $(window).scrollTop();
+
+    if (scTop < lastScrollTop && scTop > $('.header').outerHeight()){
+      backToTop.addClass('active');
+    } else {
+      backToTop.removeClass('active');
+    }
+    lastScrollTop = scTop;
+  });
+
+  $(document).on('click', '.js-back-to-top',() => {
+    $('html, body').animate({ scrollTop: 0 }, 400);
+  });
+}
+backToTop();
+
+
+
+
 $(document).on('click','.js-hamburger-menu',function() {
   $('.js-mobile-menu').toggleClass('menu-open')
-})
+});
